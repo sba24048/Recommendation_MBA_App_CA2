78052301d7bc0eb95e99699588c3b993d4b97b8e
sba24048 <sba24048@student.cct.ie>
2026-05-23 08:35:24 +0100

    Add files via upload

--- Recommendation_MBA.py
+++ Recommendation_MBA.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import scikit-learn
 from sklearn.feature_extraction.text import TfidfVectorizer   
 from sklearn.metrics.pairwise import cosine_similarity        
 
