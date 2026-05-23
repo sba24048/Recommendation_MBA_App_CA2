ebb4fb4240b473a842003347fb5fbe12b2d6fdbe
sba24048 <sba24048@student.cct.ie>
2026-05-23 09:04:15 +0100

    Update Recommendation_MBA.py

--- Recommendation_MBA.py
+++ Recommendation_MBA.py
@@ -5,7 +5,7 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer   
 from sklearn.metrics.pairwise import cosine_similarity        
 
-st.title("Recommendations & Market Basket", width="stretch")
+st.title("Recommendations & Market Basket")#, width="stretch")
 
 # Streamlit App Repository:https://github.com/sba24048/Recommendation_MBA_App_CA2
 
